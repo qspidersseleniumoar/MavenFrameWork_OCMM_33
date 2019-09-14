9addcaf03e02d34e5bdae6be5fadaa60bc447378
uday <p.udhayreddy@gmail.com>
2019-09-14 11:09:38 +0530

    my first commit

---
+++ MFrameWork/src/test/java/com/acttime/customerTest/CustomerTest.java
@@ -0,0 +1,26 @@
+package com.acttime.customerTest;
+
+
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	
+	@Test
+	public void createCustomerTest() {
+		System.out.println("execute createCustomerTest");
+	}
+	
+	
+	
+	@Test
+	public void searchCustomerTest() {
+		System.out.println("execute searchCustomerCustomerTest");
+	}
+	
+	@Test
+	public void delteCustomerTest() {
+		System.out.println("execute delteCustomerTest");
+
+	}
+
+}
